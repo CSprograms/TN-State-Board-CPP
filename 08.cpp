0e8fa1b468e5726b771f8997fbd3e601ae98d4db
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-07 10:00:05 +0530

    Create 08.cpp

---
+++ 08.cpp
@@ -0,0 +1,23 @@
+/*
+Illustration  9.9
+Reference variable
+Page : 142
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int num;
+    int &temp = num; //declaration of a reference variable temp
+
+    num = 100;
+
+    cout<<"\n The value of num = "<<num;
+    cout<<"\n The value of temp = "<<temp;
+
+return 0;
+}
+
+
