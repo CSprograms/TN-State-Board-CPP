124e0ecb417532d76f74e95a3bcc86f4eef10694
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-07 09:59:55 +0530

    Create 06.cpp

---
+++ 06.cpp
@@ -0,0 +1,24 @@
+/*
+Illustration  9.7
+Dynamic initialization
+Page : 141
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+
+    cout<<"\n Enter number 1: ";
+    cin>>num1;
+
+    cout<<"\n Enter number 2: ";
+    cin>>num2;
+
+    int sum = num1 + num2; // Dynamic Initialization
+
+    cout<<"\n Average: "<<sum/2;
+
+return 0;
+}
