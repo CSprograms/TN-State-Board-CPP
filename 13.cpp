ad2e946778ff29c635e127c2d89e631d37d97e4d
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-07 10:01:53 +0530

    Create 13.cpp

---
+++ 13.cpp
@@ -0,0 +1,33 @@
+/*
+Illustration 10.3
+Nested if statement -- Calculate commission according to grade
+Page : 157
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int sales, commission;
+    char grade;
+    cout<<"\n Enter Sales amount: ";
+    cin>>sales;
+    cout<<"\n Enter Grade: ";
+    cin>>grade;
+    if ( sales > 5000 )
+    {
+        commission = sales * 0.10;
+        cout<<"\n Commission: "<<commission;
+    }
+    else
+    {
+        commission = sales * 0.05;
+        cout<<"\n Commission: "<<commission;
+    }
+
+    cout << "\n Good Job ..... ";
+
+return 0;
+}
+
+//Input: 6000 & A
