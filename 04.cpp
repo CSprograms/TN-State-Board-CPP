874ebb0b43cec95d20d6c7312627165a1adc134e
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-07 09:59:46 +0530

    Create 04.cpp

---
+++ 04.cpp
@@ -0,0 +1,20 @@
+/*
+Garbage Value
+Page : 140
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, sum;
+
+    cout << num1 << endl;
+    cout << num2 << endl;
+
+// Modified Statement of : cout << num1 + num2;
+    sum = num1 + num2;
+    cout<<sum;
+
+return 0;
+}
