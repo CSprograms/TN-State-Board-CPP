3eee2b31ac2c7c7950ef3b0a245afb88922d5758
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-15 16:05:27 +0530

    Update 13.cpp

--- 13.cpp
+++ 13.cpp
@@ -8,12 +8,13 @@ using namespace std;
 
 int main()
 {
-    int sales, commission;
-    char grade;
+    int sales, commission;                          //  char grade;
+
     cout<<"\n Enter Sales amount: ";
-    cin>>sales;
-    cout<<"\n Enter Grade: ";
-    cin>>grade;
+    cin>>sales;                                         // cout<<"\n Enter Grade: ";    cin>>grade;
+
+// if sales greater than 5000, commission = 10% of sales; Otherwise commission = 10% of sales.
+
     if ( sales > 5000 )
     {
         commission = sales * 0.10;
