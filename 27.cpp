10dd72c4bacf1b61da55857b5e38f28a1bf0cb8f
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-16 20:00:06 +0530

    Create 27.cpp

---
+++ 27.cpp
@@ -0,0 +1,21 @@
+/*
+Program 11.1
+Accept a character and display it.
+Page : 181
+*/
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    cout<<"\n Type a Character : ";
+    char ch = getchar();
+    cout << "\n The entered Character is: ";
+    putchar(ch);
+return 0;
+}
+/*
+Output:
+Type a Character : T
+The entered Character is: T
+*/
