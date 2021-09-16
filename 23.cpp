6ba17434ca5cd0f8a8accee41f4adc141cc12ff0
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-16 19:59:52 +0530

    Create 23.cpp

---
+++ 23.cpp
@@ -0,0 +1,21 @@
+/*
+Illustration 10.13
+nested loop -- matrix multiplication
+Page : 172
+*/
+#include<iostream>
+using namespace std;
+int main(void)
+{
+    cout<< "A multiplication table:" <<endl <<"  1\t2\t3\t4\t5\t6\t7\t8\t9" <<endl<< "" <<endl;
+    for(int c = 1; c < 10; c++)
+    {
+        cout<< c << "| ";
+        for(int i = 1; i< 10; i++)
+        {
+            cout<<i * c << '\t';
+        }
+        cout<<endl;
+    }
+return 0;
+}
