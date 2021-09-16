a00571ceccdf5b6471851dc37646e31051c5e783
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-16 20:00:19 +0530

    Create 30.cpp

---
+++ 30.cpp
@@ -0,0 +1,25 @@
+/*
+Program 11.4
+Checking for Alpha-Numeric - Version 2
+Page : 184
+*/
+#include<iostream>
+#include<stdio.h>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << "\n Enter a charater: ";
+    ch = getchar();
+    cout<<"\n The Return Value of isalpha(ch) is :" << isalpha(ch) ;
+return 0;
+}
+/*
+Output-1:
+Enter a charater: A
+The Return Value of isalpha(ch) is :1
+Output-2:
+Enter a charater: 7
+The Return Value of isalpha(ch) is :0
+*/
