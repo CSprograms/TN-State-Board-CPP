77d618698722c81970017147a28b497410cb5a41
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-16 19:59:26 +0530

    Create 17.cpp

---
+++ 17.cpp
@@ -0,0 +1,16 @@
+/*
+Illustration 10.7
+for loop -- printing 0 to 9
+Page : 164
+*/
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int i;
+
+    for(i = 0; i< 10; i ++ )
+        cout<< "value of i : " <<i<<endl;
+
+return 0;
+}
