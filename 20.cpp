ab2f8e30937e8a8592f18c2709bb77fa30464ba1
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-16 19:59:39 +0530

    Create 20.cpp

---
+++ 20.cpp
@@ -0,0 +1,20 @@
+/*
+Illustration 10.10
+while loop -- sum of 1 to 10
+Page : 169
+*/
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int i=1,sum=0;
+    while(i<=10)
+    {
+        sum=sum+i;
+        i++;
+    }
+
+    cout<<"The sum of 1 to 10 is "<<sum;
+
+return 0;
+}
