99404b3e012a8ebbaf5afdc40c5aca7fecb14e24
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-16 20:00:15 +0530

    Create 29.cpp

---
+++ 29.cpp
@@ -0,0 +1,30 @@
+/*
+Program 11.3
+Checking for Alpha-Numeric
+Page : 183
+*/
+#include<iostream>
+#include<stdio.h>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+    char ch;
+    int r;
+
+    cout<<"\n Type a Character :";
+    ch = getchar();
+
+    r = isalnum(ch);
+
+    cout<<"\nThe Return Value of isalnum(ch) is :"<<r;
+return 0;
+}
+/*
+Output-1:
+Type a Character :A
+The Return Value of isalnum(ch) is :1
+Output-2:
+Type a Character :?
+The Return Value of isalnum(ch) is :0
+*/
