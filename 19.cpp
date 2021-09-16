f505a1fbc5b91a9ac671c4d73ec8606228e8ea46
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-16 19:59:35 +0530

    Create 19.cpp

---
+++ 19.cpp
@@ -0,0 +1,23 @@
+/*
+Illustration 10.9
+for loop -- sum of 1 to n
+Page : 166
+*/
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int i, sum=0, n;
+
+    cout<<"\n Enter The value of n";
+    cin>>n; i =1;
+
+    for (  ; i<=n;  )
+    {
+        sum += i;
+        ++i;
+    }
+
+    cout<<"\n The sum of 1 to " <<n<<"is "<<sum;
+return 0;
+}
