142b3818ccecf76c91f80bd923439acad39e75f4
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-16 20:00:32 +0530

    Create 33.cpp

---
+++ 33.cpp
@@ -0,0 +1,18 @@
+/*
+Program 11.7
+Built-In Function - strlen(string)
+Page : 186
+*/
+#include<string.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    char source[ ] = "Computer Science";
+    cout<<"\n Given String is "<<source<<" its Length is "<<strlen(source);
+return 0;
+}
+/*
+Output:
+Given String is Computer Science its Length is 16
+*/
