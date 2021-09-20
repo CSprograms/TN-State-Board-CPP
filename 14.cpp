be2afce73c041bf601a5aaac1e079163a9ee5632
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-20 12:07:06 +0530

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -12,18 +12,27 @@ int main()
 
     cout<<"Enter the Marks : ";
     cin>>marks;
-
+    if ( marks >=0 && marks <=100 )
+    {
     if ( marks >= 60 )
+    {
         cout<<"Your grade is 1st class !!"<<endl;
-
-    else if ( marks >= 50 && marks < 60 )
+    }
+    else if ( marks >= 50 )     //  else if ( marks >= 50 && marks < 60 )
+    {
         cout<<"Your grade is 2nd class !!"<<endl;
-
-    else if ( marks >= 40 && marks < 50 )
+    }
+    else if ( marks >= 40 )     //  else if ( marks >= 40 && marks < 50 )
+    {
         cout<<"Your grade is 3rd class !!"<<endl;
-
+    }
     else
+    {
         cout<<"You are fail !!"<<endl;
+    }
+    }
+    else
+        cout<<"Mark is out of range"<<endl;
 
 return 0;
 }
