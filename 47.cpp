885ee585aed7df0b20114f1d74cd94ffa5f3a8b9
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-22 05:50:39 +0530

    Create 47.cpp

---
+++ 47.cpp
@@ -0,0 +1,23 @@
+/*
+Program 11.20
+Function without return value and without parameter
+Page : 197
+*/
+#include<iostream>
+using namespace std;
+
+void display()
+{
+    cout<<"First C++ Program with Function";
+}
+
+int main()
+{
+     display(); // Function calling statement//
+
+return 0;
+}
+/*
+Output:
+First C++ Program with Function
+*/
