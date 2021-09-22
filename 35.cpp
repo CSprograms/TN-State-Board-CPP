341fcfa07b8716e5588111177860754689edf76b
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-22 05:49:46 +0530

    Create 35.cpp

---
+++ 35.cpp
@@ -0,0 +1,20 @@
+/*
+Program 11.9
+Built-In Function - strcat(target, source)
+Page : 188
+*/
+#include<string.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    char target[50] = "Learning C++ is fun";
+	char source[50] = " , easy and Very useful";
+	strcat(target, source);
+
+	cout << target ; return 0;
+}
+/*
+Output
+Learning C++ is fun , easy and Very useful
+*/
