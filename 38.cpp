43479a8262633992bee1d7c3d010a17ad25bb310
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-22 05:50:00 +0530

    Create 38.cpp

---
+++ 38.cpp
@@ -0,0 +1,22 @@
+/*
+Program 11.12
+Built-In Function - cos()
+Page : 189
+*/
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    double x = 0.5, result;
+
+    result = cos(x);
+
+    cout << "COS("<<x<<")= "<<result;
+
+return 0;
+}
+/*
+Output
+COS(0.5)= 0.877583
+*/
