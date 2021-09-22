b4138e47602eaf8a979714269899761bb5d398e4
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-22 05:50:06 +0530

    Create 39.cpp

---
+++ 39.cpp
@@ -0,0 +1,22 @@
+/*
+Program 11.13
+Built-In Function - sqrt()
+Page : 190
+*/
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    double x = 625, result;
+
+    result = sqrt(x);
+
+    cout << "sqrt("<<x<<") = "<<result;
+
+return 0;
+}
+/*
+Output
+sqrt(625) = 25
+*/
