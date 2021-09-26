366dcb22355306eee973ab5f86fb7800ce8c9645
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-26 12:24:49 +0530

    Update 31.cpp

--- 31.cpp
+++ 31.cpp
@@ -10,9 +10,12 @@ using namespace std;
 int main()
 {
     char ch;
+
     cout << "\n Enter a Character: ";
     cin >> ch;
+
     cout<<"\n The Return Value of isdigit(ch) is :" << isdigit(ch) ;
+
 return 0;
 }
 /*
