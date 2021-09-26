6dc074472644867fd9bf52b39cc7e47e311bab8c
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-26 12:24:19 +0530

    Update 24.cpp

--- 24.cpp
+++ 24.cpp
@@ -8,12 +8,15 @@ using namespace std;
 int main()
 {
     int n=1;
+
 jump:
     {
         if(n<10)
         {
             cout<<n<<'\t';
+
             n+=2;
+
             goto jump;          // Control of the program move to jump:
         }
         else
