d089d56ae5eeaa4987e90c6855af3de553944d06
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-26 12:24:27 +0530

    Update 26.cpp

--- 26.cpp
+++ 26.cpp
@@ -4,16 +4,20 @@ continue -- Printing 1 to 10, except 6
 Page : 176
 */
 #include <iostream>
-using namespace std;  
+using namespace std;
 int main()
 {
+
     for (int i = 1; i<= 10; i++)
     {
+
         if (i == 6)
             continue;
         else
             cout<<i<< " ";
+
     }
+
 return 0;
 }
 /*
