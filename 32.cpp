983079b6700e6cbe856e5cc9040d259c4ab9d8e1
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-09-26 12:24:53 +0530

    Update 32.cpp

--- 32.cpp
+++ 32.cpp
@@ -10,9 +10,15 @@ int main()
 {
     char source[] = "Computer Science";
     char target[20]="target";
+
     cout<<"\n String in Source Before Copied :"<<source;
-    cout<<"\n String in Target Before Copied :"<<target; strcpy(target,source);
+
+    cout<<"\n String in Target Before Copied :"<<target;
+
+    strcpy(target,source);
+
     cout<<"\n String in Target After strcpy function Executed :"<<target;
+
 return 0;
 }
 /*
