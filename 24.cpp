533335799a1887854e104b1e42667e2c044558cb
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-10-04 00:11:20 +0530

    Update 24.cpp

--- 24.cpp
+++ 24.cpp
@@ -14,10 +14,10 @@ jump:
 
     cout<<n<<'\t';
 
-    n+=2;
+    n += 2;
 
-    if(n<10)
-        goto jump;          // Control of the program move to jump:
+    if( n < 10 )
+        goto jump;
 
 return 0;
 }
