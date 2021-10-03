e06bfa82b08e3defc456c948627fc0c0d8e4f641
CSprograms <aravindhanmohan.tvm@gmail.com>
2021-10-04 00:11:27 +0530

    Update 26.cpp

--- 26.cpp
+++ 26.cpp
@@ -8,13 +8,13 @@ using namespace std;
 int main()
 {
 
-    for (int i = 1; i<= 10; i++)
+    for ( int i = 1; i <= 10; i++ )
     {
 
         if (i == 6)
             continue;
-        else
-            cout<<i<< " ";
+
+        cout<<i<< " ";
 
     }
 
